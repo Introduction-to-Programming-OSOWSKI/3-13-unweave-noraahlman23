e78d7b9fd98b97a82077b589ba4fc22e094c680a
noraahlman23 <819446@mystma.org>
2021-02-26 16:08:58 +0000

    unweave

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 def unweave(w):
-    newword1 = " "
+    newword1 = ""
     newword2 = " "
     for i in range (0, len(w), 2):
         newword1 = newword1 + w[i]
 
     for i in range (1, len(w), 2):
         newword2 = newword2 + w[i]
-    return newword1 + " " + newword2
+    return newword1 + newword2
 print (unweave("potato"))
